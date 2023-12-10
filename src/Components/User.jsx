b5aab95db331e0e48173f1c1a0f4ce48a60742b9
Suraj <suraj8423tripathi@gmail.com>
2023-12-11 03:12:49 +0530

    comparison between class component and functional component in making state

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useState } from "react";
 
 const User = (props) => {
+  const [count] = useState(0);
+  const [count2] = useState(1);
   return (
     <div className="user-card">
+      <h1>Count = {count}</h1>
+      <h1>Count2 = {count2}</h1>
       <h2>Name : {props.name}</h2>
       <h3>Locations : Varanasi</h3>
       <h4>Contact : st172486</h4>
